26f09940efa9ba4ef2692c587b8780d6b0dce73d
Nicolas <nicolas.cartes.genta@gmail.com>
2022-08-30 22:04:49 -0400

    Create my first version of multi viewer object

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment } from '@react-three/drei'
 
-import ModelViewer from './components/ModelViewer';
+//import ModelViewer from './components/ModelViewer';
+import MultiModelViewer from './components/MultiModelViewer';
 
 function App() {
   return (
     <div className="App">
-      <ModelViewer scale="1" modelPath={"../models/Drill2.glb"} />
+      <MultiModelViewer scale="4" modelPath={"../models/Drill2.glb"} scale2="1" modelPath2={"../models/Orbit7.glb"} />
     </div>
   );
 }
